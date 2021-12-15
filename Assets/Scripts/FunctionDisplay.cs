195a00d9544cef7512647bb2a41b72fdc7a6356c
GuillaumeCharvet <guicharvet@orange.fr>
2021-12-15 11:18:57 +0100

    Condition Victoire

--- Assets/Scripts/FunctionDisplay.cs
+++ Assets/Scripts/FunctionDisplay.cs
@@ -12,14 +12,22 @@ public class FunctionDisplay : MonoBehaviour
 
     public float parameter_a = 0f;
     public float parameter_b = 0f;
+
+    public float parameter_a0 = 0f;
+    public float parameter_b0 = 0f;
+
     private Color color_trace1 = new Color (0,1,0, 1f);
     private Color color_trace2 = new Color (0.2f,0.35f,0.2f, 1f);
 
     public float random_start_value_a;
     public float random_start_value_b;
 
+    public bool game_won = false;
+
     [SerializeField] private Sprite circleSprite;
     private RectTransform graphContainer;
+
+    public bool parameters_changed = false;
 
     private void Awake()
     {
@@ -27,16 +35,25 @@ public class FunctionDisplay : MonoBehaviour
         random_start_value_a = Random.Range(0f, 6.28f);
         random_start_value_b = Random.Range(0.3f, 1f);
         //List<float> valueList = new List<float>() {5, 98, 56, 45, 30, 22, 14, 10, 15, 13, 12, 11, 37};
+
+        valueList = CreateList(random_start_value_a, random_start_value_b);
+        ShowGraph(valueList, color_trace2, 11f);
+        valueList = CreateList(parameter_a, parameter_b);
+        ShowGraph(valueList, color_trace1, 3f);
     }
 
     private void Update()
     {
-        Clear();
-        valueList = CreateList(random_start_value_a, random_start_value_b);
-        ShowGraph(valueList, color_trace2, 11f);
-        //parameter_a += 0.1f;
-        valueList = CreateList(parameter_a, parameter_b);
-        ShowGraph(valueList, color_trace1, 3f);
+        if (parameter_a0 != parameter_a || parameter_b0 != parameter_b)
+        {
+            Clear();
+            valueList = CreateList(random_start_value_a, random_start_value_b);
+            ShowGraph(valueList, color_trace2, 11f);
+            valueList = CreateList(parameter_a, parameter_b);
+            ShowGraph(valueList, color_trace1, 3f);
+        }
+        parameter_a0 = parameter_a;
+        parameter_b0 = parameter_b;
     }
 
     private void Clear()
@@ -46,26 +63,24 @@ public class FunctionDisplay : MonoBehaviour
             if (child.name != "background"){GameObject.Destroy(child.gameObject);};
         }
     }
-
     public void Change_Value_a(float button_value)
     {
-        parameter_a = button_value * 6.28f;
+        if (!game_won) { parameter_a = button_value * 6.28f; }
     }
     public void Change_Value_b(float button_value)
     {
-        parameter_b = 0.1f + button_value * .9f;
+        if (!game_won) { parameter_b = 0.1f + button_value * .9f; }
     }
-
     private List<float> CreateList(float parameter_a, float parameter_b)
     {
         List<float> valueList = new List<float>() {};
         for (int i = 0; i < timeStep; i++)
         {
-            valueList.Add( 50 + 25 * Mathf.Sin(deltaT*i + parameter_a) + parameter_b * 16 * Mathf.Sin(2f*deltaT*i));
+            valueList.Add( 50 + 25 * Mathf.Sin((deltaT*80/timeStep)*i + parameter_a) + parameter_b * 16 * Mathf.Sin(2f*(deltaT*80/timeStep)*i));
         }
         return valueList;
     }
-    private GameObject CreateCircle(Vector2 anchoredPosition)
+    /*private GameObject CreateCircle(Vector2 anchoredPosition)
     {
         GameObject gameObject = new GameObject("circle", typeof(Image));
         gameObject.transform.SetParent(graphContainer, false);
@@ -76,8 +91,8 @@ public class FunctionDisplay : MonoBehaviour
         rectTransform.anchorMin = new Vector2(0, 0);
         rectTransform.anchorMax = new Vector2(0, 0);
         return gameObject;
-    }
-    private void ShowGraph(List<float> valueList, Color color, float sizeDelta)
+    }*/
+    /*private void ShowGraph(List<float> valueList, Color color, float sizeDelta)
     {
         float graphHeight = graphContainer.sizeDelta.y;
         float yMaximum = 100f;
@@ -94,6 +109,41 @@ public class FunctionDisplay : MonoBehaviour
                 CreateDotConnection(lastCircleGameObject.GetComponent<RectTransform>().anchoredPosition, circleGameObject.GetComponent<RectTransform>().anchoredPosition, color, sizeDelta);
             }
             lastCircleGameObject = circleGameObject;
+        }
+    }
+    private void CreateDotConnection(Vector2 dotPositionA, Vector2 dotPositionB, Color color, float sizeDelta)
+    {
+        GameObject gameObject = new GameObject("dotConnection", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().color = color;
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        Vector2 dir = (dotPositionB - dotPositionA).normalized;
+        float distance = Vector2.Distance(dotPositionA, dotPositionB);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+        rectTransform.sizeDelta = new Vector2(distance, sizeDelta);
+        rectTransform.anchoredPosition = dotPositionA + dir * distance * .5f;
+        rectTransform.localEulerAngles = new Vector3(0, 0, Vector2.SignedAngle(new Vector2(1, 0), dir));
+    }*/
+    private void ShowGraph(List<float> valueList, Color color, float sizeDelta)
+    {
+        float graphHeight = graphContainer.sizeDelta.y;
+        float yMaximum = 100f;
+        float graphWidth = graphContainer.sizeDelta.x;
+        float xSize = graphWidth / timeStep;
+
+        Vector2 lastVectorObject = new Vector2(-100000f,-100000f);
+        for (int i = 0; i < valueList.Count; i++)
+        {
+            float xPosition = (i + 1) * xSize;
+            float yPosition = (valueList[i] / yMaximum) * graphHeight;
+            Vector2 vectorObject = new Vector2(xPosition, yPosition);
+            //GameObject circleGameObject = CreateCircle(new Vector2(xPosition, yPosition));
+            if (lastVectorObject != new Vector2(-100000f, -100000f))
+            {
+                CreateDotConnection(lastVectorObject, vectorObject, color, sizeDelta);
+            }
+            lastVectorObject = vectorObject;
         }
     }
     private void CreateDotConnection(Vector2 dotPositionA, Vector2 dotPositionB, Color color, float sizeDelta)
--- Assets/Scripts/VictoryCondition.cs
+++ Assets/Scripts/VictoryCondition.cs
@@ -5,17 +5,18 @@ using UnityEngine.UI;
 
 public class VictoryCondition : MonoBehaviour
 {
-
     private float a;
     private float b;
     private FunctionDisplay child;
-    private Text text;
+    public Text text;
+    private Scrollbar scrollbar;
 
     // Start is called before the first frame update
     void Start()
     {
         child = transform.Find("WindowGraph").GetComponent<FunctionDisplay>();
         text = transform.Find("Text").GetComponent<Text>();
+        scrollbar = transform.Find("Scrollbar").GetComponent<Scrollbar>();
 
         text.enabled = false;
     }
@@ -23,10 +24,14 @@ public class VictoryCondition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Mathf.Abs(child.parameter_a - child.random_start_value_a)/6.28f + Mathf.Abs(child.parameter_b - child.random_start_value_b) < 0.01f)
+        if (!child.game_won)
         {
-            text.enabled = true;
+            if (Mathf.Abs(child.parameter_a - child.random_start_value_a) / 6.28f + Mathf.Abs(child.parameter_b - child.random_start_value_b) < 0.01f)
+            {
+                text.enabled = true;
+                child.game_won = true;
+                scrollbar.enabled = false;
+            }
         }
-
     }
 }
